670cd2fe7e69e8a3f631ec335e2b0057a4e8e1c7
elyasaflehrer <0548491959l@gmail.com>
2026-02-23 00:38:35 +0200

    supported data structured

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -7,11 +9,27 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The url of the source")
+    job_title:str = Field(description="The title of the job")
+
+class Titles(BaseModel):
+    """Schema for a titles used by the agent"""
+    job_title:str = Field(description="The title of the job")
+
+class AgentResponse(BaseModel):
+    """Scheme for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources used to genereted the answer")
+    titles:List[Titles] = Field(default_factory=List,description="List of jobs Titles used to genereted the answer")
+
+
 load_dotenv()
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm,tools=tools)
+agent = create_agent(model=llm,tools=tools,response_format=AgentResponse)
 def main():
     print("Hello from langchain-ai-agents!")
     result = agent.invoke({"messages": HumanMessage(content="Search for 3 jobs postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
